9d7898c687a615adbd0a7c216445d0d51b33f376
andrewinkler <andre.winkler@web.de>
2018-03-30 07:58:21 +0200

    UPDATE: Removed import.

--- src/main/java/de/awtools/basic/AWTools.java
+++ src/main/java/de/awtools/basic/AWTools.java
@@ -40,7 +40,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.Validate;
